e4cc546bf42a488969aee7b1e54b58fd985562c9
SpeedKenode <speedkenode@gmail.com>
2025-05-17 12:27:48 +0700

    upd

---
+++ cogs/moderation.py
@@ -0,0 +1,76 @@
+import discord
+from discord.ext import commands
+from discord.ext.commands.errors import MissingPermissions, MissingRequiredArgument, BadArgument
+
+class moderation(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+    
+    @commands.command()
+    async def kick(self, ctx, member: discord.Member, *, reason=None):
+        await member.kick(reason=reason)
+        await ctx.send("In development")
+
+    @commands.command()
+    async def ban(self, ctx):
+        await ctx.send("In development")
+
+    @commands.command()
+    async def unban(self, ctx):
+        await ctx.send("In development")
+
+    @commands.command()
+    async def mute(self, ctx):
+        await ctx.send("In development")
+
+    @commands.command()
+    async def unmute(self, ctx):
+        await ctx.send("In development")
+
+    @commands.command()
+    async def warn(self, ctx):
+        await ctx.send("In development")
+
+    @commands.hybrid_command()
+    @commands.has_permissions(manage_messages=True)
+    async def purge(self, ctx: commands.Context, amount: int):
+        """
+        Delete amount of message
+
+        Parameters
+        ----------
+        ctx: commands.Context
+            The context of the command invocation
+        amount: int
+            The number of message
+        """
+        if amount > 0:
+            try:
+                await ctx.message.delete()
+            except:
+                print()
+            if amount > 10:
+                message = await ctx.send("Reach the limit")
+                await message.delete(delay=2)
+                amount = 10
+            deleted = await ctx.channel.purge(limit = amount)
+            await ctx.defer()
+            message = await ctx.send(f"Done. Delete {len(deleted)} messages.")
+            await message.delete(delay=2)
+        else:
+            await ctx.send("Dafuq")
+
+    @purge.error
+    async def purge_error(self, ctx: commands.Context, error):
+        if isinstance(error, MissingPermissions):
+            await ctx.send("You don't have permission")
+        elif isinstance(error, MissingRequiredArgument):
+            await ctx.send("add number to execute command")
+        elif isinstance(error, BadArgument):
+            await ctx.send("PLS use number you dumbsh#t")
+        else:
+            print(error)
+
+
+async def setup(bot):
+    await bot.add_cog(moderation(bot))
